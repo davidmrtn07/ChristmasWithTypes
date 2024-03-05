fefacb2a170b82dfc761701e5de7e43ac7a2f8cd
davidmrtn07 <davidmrtn07@gmail.com>
2024-03-04 20:12:56 -0600

    Corrections Made

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -10,17 +10,17 @@ namespace ChristmasWithTypes
 
             var xmasDay = Christmas.Day.Thursday;
 
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = Kris Kringle;
+            //DONE set Santa's name to Kris Kringle
+            xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{Iphone, computer, Car}; 
+            //DONE Insert 3 presents you would like for xmas.  They must be strings
+            xmas.Presents =  new string[3]{ "IPhone", "MacBook", "Silverado" }; 
 
-            //TODO Set the TreeHeight to 10
+            //Done Set the TreeHeight to 10
             xmas.TreeHeight = 10;
 
-            Console.WriteLine($"This year christmas falls on {Monday} \n");
-            Console.WriteLine($"Our tree will be {xmas.10ft} feet high \n");
+            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
+            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
             Console.WriteLine("Here are the presents we would like:");
 
             foreach (var present in xmas.Presents)
@@ -28,7 +28,7 @@ namespace ChristmasWithTypes
                 Console.WriteLine($"    {present}");
             }
 
-            Console.WriteLine($"\n We like to call Santa, {Kris Kringle}");
+            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
         }
     }
 }
